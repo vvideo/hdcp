6eba284732f6196fa7b0cb80bcc701eb57f7f3b4
Denis Seleznev <hcodes@users.noreply.github.com>
2024-04-05 22:39:59 +0300

    :cyclone:

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,7 @@
 export declare const hdcpVersions: string[];
 export declare function checkHdcpVersion(keySystem: string, version: string): Promise<MediaKeyStatus>;
-interface CheckHdcpVersion {
+export interface CheckHdcpVersion {
     version: string;
     status: MediaKeyStatus;
 }
 export declare function checkAllHdcpVersions(keySystem: string): Promise<CheckHdcpVersion[]>;
-export {};
