290d094d2529deaa2e64f361d60bdf68d2695475
Denis Seleznev <hcodes@users.noreply.github.com>
2025-11-23 21:59:53 +0300

    🌀

--- dist/index.common.js
+++ dist/index.common.js
@@ -9,10 +9,11 @@ const hdcpVersions = [
     '1.4',
     '2.0',
     '2.1',
-    '2.2', // Ultra HD 4K
-    '2.3',
+    '2.2', // 4K
+    '2.3', // 8K
 ];
-const HDCP_MIN_VERSION_WITH_UHD = '2.2';
+const HDCP_MIN_VERSION_WITH_4K = '2.2';
+const HDCP_MIN_VERSION_WITH_8K = '2.3';
 function getMaxHdcpVersion(versions) {
     for (let i = versions.length - 1; i >= 0; i--) {
         const item = versions[i];
@@ -22,10 +23,16 @@ function getMaxHdcpVersion(versions) {
     }
     return '';
 }
-function isUhdHdcpSupported(versions) {
+function is4KHdcpSupported(versions) {
     const maxVersion = getMaxHdcpVersion(versions);
     return maxVersion ?
-        parseFloat(maxVersion) >= parseFloat(HDCP_MIN_VERSION_WITH_UHD) :
+        parseFloat(maxVersion) >= parseFloat(HDCP_MIN_VERSION_WITH_4K) :
+        false;
+}
+function is8KHdcpSupported(versions) {
+    const maxVersion = getMaxHdcpVersion(versions);
+    return maxVersion ?
+        parseFloat(maxVersion) >= parseFloat(HDCP_MIN_VERSION_WITH_8K) :
         false;
 }
 const defaultConfig = [{
@@ -47,7 +54,6 @@ function checkHdcpVersion(keySystem, version) {
             error.name = 'NotSupportedError';
             throw error;
         }
-        // @ts-ignore
         return mediaKeys.getStatusForPolicy({ minHdcpVersion: version });
     });
 }
@@ -67,9 +73,7 @@ function checkAllHdcpVersions(keySystem) {
         }
         const promises = [];
         hdcpVersions.forEach(minHdcpVersion => {
-            promises.push(
-            // @ts-ignore
-            mediaKeys.getStatusForPolicy({ minHdcpVersion }).then(status => ({
+            promises.push(mediaKeys.getStatusForPolicy({ minHdcpVersion }).then(status => ({
                 version: minHdcpVersion,
                 status,
             })));
@@ -77,10 +81,50 @@ function checkAllHdcpVersions(keySystem) {
         return Promise.all(promises);
     });
 }
+async function findMaxHdcpVersion(keySystem) {
+    if (typeof window.navigator.requestMediaKeySystemAccess !== 'function') {
+        const error = new Error('navigator.requestMediaKeySystemAccess is not supported');
+        error.name = 'NotSupportedError';
+        return Promise.reject(error);
+    }
+    const mediaKeys = await window.navigator.requestMediaKeySystemAccess(keySystem, defaultConfig)
+        .then(mediaKeySystemAccess => mediaKeySystemAccess.createMediaKeys());
+    if (!('getStatusForPolicy' in mediaKeys)) {
+        const error = Error('Method getStatusForPolicy is not supported');
+        error.name = 'NotSupportedError';
+        throw error;
+    }
+    let left = 0;
+    let right = hdcpVersions.length - 1;
+    let maxSupportedVersion = '';
+    const maxVersion = hdcpVersions[right];
+    const maxStatus = await mediaKeys.getStatusForPolicy({ minHdcpVersion: maxVersion });
+    if (maxStatus === 'usable') {
+        return maxVersion;
+    }
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2);
+        const middleVersion = hdcpVersions[middle];
+        const midStatus = await mediaKeys.getStatusForPolicy({ minHdcpVersion: middleVersion });
+        if (midStatus === 'usable') {
+            // Если текущая версия поддерживается, сохраняем её и ищем более высокую
+            maxSupportedVersion = middleVersion;
+            left = middle + 1;
+        }
+        else {
+            // Если текущая версия не поддерживается, ищем более низкую
+            right = middle - 1;
+        }
+    }
+    return maxSupportedVersion;
+}
 
-exports.HDCP_MIN_VERSION_WITH_UHD = HDCP_MIN_VERSION_WITH_UHD;
+exports.HDCP_MIN_VERSION_WITH_4K = HDCP_MIN_VERSION_WITH_4K;
+exports.HDCP_MIN_VERSION_WITH_8K = HDCP_MIN_VERSION_WITH_8K;
 exports.checkAllHdcpVersions = checkAllHdcpVersions;
 exports.checkHdcpVersion = checkHdcpVersion;
+exports.findMaxHdcpVersion = findMaxHdcpVersion;
 exports.getMaxHdcpVersion = getMaxHdcpVersion;
 exports.hdcpVersions = hdcpVersions;
-exports.isUhdHdcpSupported = isUhdHdcpSupported;
+exports.is4KHdcpSupported = is4KHdcpSupported;
+exports.is8KHdcpSupported = is8KHdcpSupported;
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,4 +1,3 @@
-// https://wicg.github.io/hdcp-detection/
 const hdcpVersions = [
     '1.0',
     '1.1',
@@ -7,10 +6,11 @@ const hdcpVersions = [
     '1.4',
     '2.0',
     '2.1',
-    '2.2', // Ultra HD 4K
+    '2.2',
     '2.3',
 ];
-const HDCP_MIN_VERSION_WITH_UHD = '2.2';
+const HDCP_MIN_VERSION_WITH_4K = '2.2';
+const HDCP_MIN_VERSION_WITH_8K = '2.3';
 function getMaxHdcpVersion(versions) {
     for (let i = versions.length - 1; i >= 0; i--) {
         const item = versions[i];
@@ -20,10 +20,16 @@ function getMaxHdcpVersion(versions) {
     }
     return '';
 }
-function isUhdHdcpSupported(versions) {
+function is4KHdcpSupported(versions) {
     const maxVersion = getMaxHdcpVersion(versions);
     return maxVersion ?
-        parseFloat(maxVersion) >= parseFloat(HDCP_MIN_VERSION_WITH_UHD) :
+        parseFloat(maxVersion) >= parseFloat(HDCP_MIN_VERSION_WITH_4K) :
+        false;
+}
+function is8KHdcpSupported(versions) {
+    const maxVersion = getMaxHdcpVersion(versions);
+    return maxVersion ?
+        parseFloat(maxVersion) >= parseFloat(HDCP_MIN_VERSION_WITH_8K) :
         false;
 }
 const defaultConfig = [{
@@ -45,7 +51,6 @@ function checkHdcpVersion(keySystem, version) {
             error.name = 'NotSupportedError';
             throw error;
         }
-        // @ts-ignore
         return mediaKeys.getStatusForPolicy({ minHdcpVersion: version });
     });
 }
@@ -65,9 +70,7 @@ function checkAllHdcpVersions(keySystem) {
         }
         const promises = [];
         hdcpVersions.forEach(minHdcpVersion => {
-            promises.push(
-            // @ts-ignore
-            mediaKeys.getStatusForPolicy({ minHdcpVersion }).then(status => ({
+            promises.push(mediaKeys.getStatusForPolicy({ minHdcpVersion }).then(status => ({
                 version: minHdcpVersion,
                 status,
             })));
@@ -75,5 +78,40 @@ function checkAllHdcpVersions(keySystem) {
         return Promise.all(promises);
     });
 }
+async function findMaxHdcpVersion(keySystem) {
+    if (typeof window.navigator.requestMediaKeySystemAccess !== 'function') {
+        const error = new Error('navigator.requestMediaKeySystemAccess is not supported');
+        error.name = 'NotSupportedError';
+        return Promise.reject(error);
+    }
+    const mediaKeys = await window.navigator.requestMediaKeySystemAccess(keySystem, defaultConfig)
+        .then(mediaKeySystemAccess => mediaKeySystemAccess.createMediaKeys());
+    if (!('getStatusForPolicy' in mediaKeys)) {
+        const error = Error('Method getStatusForPolicy is not supported');
+        error.name = 'NotSupportedError';
+        throw error;
+    }
+    let left = 0;
+    let right = hdcpVersions.length - 1;
+    let maxSupportedVersion = '';
+    const maxVersion = hdcpVersions[right];
+    const maxStatus = await mediaKeys.getStatusForPolicy({ minHdcpVersion: maxVersion });
+    if (maxStatus === 'usable') {
+        return maxVersion;
+    }
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2);
+        const middleVersion = hdcpVersions[middle];
+        const midStatus = await mediaKeys.getStatusForPolicy({ minHdcpVersion: middleVersion });
+        if (midStatus === 'usable') {
+            maxSupportedVersion = middleVersion;
+            left = middle + 1;
+        }
+        else {
+            right = middle - 1;
+        }
+    }
+    return maxSupportedVersion;
+}
 
-export { HDCP_MIN_VERSION_WITH_UHD, checkAllHdcpVersions, checkHdcpVersion, getMaxHdcpVersion, hdcpVersions, isUhdHdcpSupported };
+export { HDCP_MIN_VERSION_WITH_4K, HDCP_MIN_VERSION_WITH_8K, checkAllHdcpVersions, checkHdcpVersion, findMaxHdcpVersion, getMaxHdcpVersion, hdcpVersions, is4KHdcpSupported, is8KHdcpSupported };
